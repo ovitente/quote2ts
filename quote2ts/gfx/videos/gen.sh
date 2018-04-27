8727854c680cd590fd830505c4432ad9327cf1be
Ivan Kostrubin <7spawn@gmail.com>
2018-04-27 17:03:47 +0300

    creds loading fixes

---
+++ quote2ts/gfx/videos/gen.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd ./prototype
+#for i in $(ls); do convert -scale 40% $i $i; done;
+for i in $(ls); do convert $i -bordercolor black -border 1 $i; done;
+for i in $(ls); do convert -append $i ../under-line.png $i; done;
